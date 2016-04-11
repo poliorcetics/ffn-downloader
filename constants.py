f529840fe49b7fe7652abf22ff64fad22575fe31
poliorcetics <user@MBAlexis.local>
2016-04-11 22:56:04 +0200

    Typo.
    To improve readability, in the file and in the shell.

--- constants.py
+++ constants.py
@@ -7,26 +7,38 @@ author: Poliorcetics
 Contains all the constants used in the second version of the FFN-DOWNLOADER.
 
  - INTRO
+ - ASK_URL
  - ROOT_URL
  - HEADER
- - ASK_URL
 """
+
 
 INTRO = """ -- FFN-DOWNLOADER --
 
-This program will compile any story coming from https://www.fanfiction.net/ in
-multiples html files.
+    This program will compile any story coming from https://www.fanfiction.net/
+in multiples html files.
 
 Files created:
- - one .hmtl file per chapter, named as following: [number]. [chapter_title]\
-.html
- - one .html file for the full story, named as following: [story_title].html
+    - one .hmtl file per chapter, named as following: [number]. [chapter_\
+title].html
+    - one .html file for the full story, named as following: [story_title].html
 
-Each file will get a header to include some CSS and formatting to make it
+    Each file will get a header to include some CSS and formatting to make it
 easier to read. If you're not content with what I choosed, just edit the
 default parameters of the constant 'HEADER'."""
 
+
+ASK_URL = """
+    Please enter the URL of the first chapter of the wanted story.
+The URL should have the following format:
+    -> https://www.fanfiction.net/s/[STORY_ID]/1/[NAME]
+Like in this:
+    -> https://www.fanfiction.net/s/4641394/1/The-Substitute
+For the [NAME], replace spaces ' ' by '-' if needed."""
+
+
 ROOT_URL = "https://www.fanfiction.net/"
+
 
 HEADER = """<!DOCTYPE html>
 <html>
@@ -49,13 +61,4 @@ HEADER = """<!DOCTYPE html>
     </style>
 </head>
 <body>
-"""
-
-ASK_URL = """
-Please enter the URL of the first chapter of the wanted story.
-The URL should have the following format:
-https://www.fanfiction.net/s/[STORY_ID]/1/[NAME]
-Like in this:
-https://www.fanfiction.net/s/4641394/1/The-Substitute
-For the [NAME], replace spaces ' ' by '-' if needed.
 """
